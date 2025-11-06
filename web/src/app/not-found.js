4deeb99aa6620d8d67d611307f3d0faa84fcbffa
vetlesen <jonas@vetlesen.no>
2025-11-06 19:07:14 +0100

    feat: update

--- web/src/app/not-found.js
+++ web/src/app/not-found.js
@@ -5,8 +5,8 @@ export default function NotFound() {
     <main className="px-4 mt-52 min-h-[90svh] flex flex-col max-w-lg">
       <h1 className="mb-4">404 - Page Not Found</h1>
       <p className="mb-6 font-normal">
-        The page you're looking for doesn't exist or you don't have permission
-        to view it.
+        The page youre looking for doesnt exist or you dont have permission to
+        view it.
       </p>
       <Link href="/" className="font-normal underline">
         Return to Home
